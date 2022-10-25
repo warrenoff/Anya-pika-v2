93884f82c7589da54d2cd774c19137ab4ea60074
3.69_pika <106071901+Pika4O4@users.noreply.github.com>
2022-10-25 23:16:33 +0530

    Update AnyaCore.js

--- AnyaCore.js
+++ AnyaCore.js
@@ -1,4 +1,4 @@
-require('./settings')
+require('./settings','./nexus')
 const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require('@adiwajshing/baileys')
 const fs = require('fs')
 const util = require('util')
