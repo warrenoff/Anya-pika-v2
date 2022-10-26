7e0379fab078ac2ab6c14b103046da949d314c01
3.69_pika <106071901+Pika4O4@users.noreply.github.com>
2022-10-26 12:57:02 +0530

    Update AnyaCore.js

--- AnyaCore.js
+++ AnyaCore.js
@@ -496,8 +496,8 @@ const reply = (teks) => {
 	
 	//antispam or auto react
 //if (m.message && msgFilter.isFiltered(from)) {
-//console.log(`${global.themeemoji}[SPAM]`, color(moment(m.messageTimestamp * 1000).format('DD/MM/YYYY HH:mm:ss'), 'yellow'), color(`${command} [${args.length}]`), 'from', color(m.pushName))
-//return AnyaPika.sendMessage(from, { react: { text: `${global.themeemoji}`, key: m.key }})
+//console.log(`${globalthemeemoji}[SPAM]`, color(moment(m.messageTimestamp * 1000).format('DD/MM/YYYY HH:mm:ss'), 'yellow'), color(`${command} [${args.length}]`), 'from', color(m.pushName))
+//return AnyaPika.sendMessage(from, { react: { text: `${globalthemeemoji}`, key: m.key }})
 //}
 	
 //auto read whatsapp status
@@ -751,7 +751,7 @@ let docs = pickRandom(documents)
 let symbols = [symb1,symb2,symb3,symb4,symb5,symb6,symb7,symb8,symb9,symb10]
 let symb = pickRandom(symbols)
 let themeemojimsg = [global.themeemoji1,global.themeemoji2,global.themeemoji3,global.themeemoji4,global.themeemoji5,global.themeemoji6,global.themeemoji7,global.themeemoji8,global.themeemoji9,global.themeemoji10]
-let global.themeemoji = pickRandom(themeemojimsg)
+let globalthemeemoji = pickRandom(themeemojimsg)
 let menuimages = [thumb1,thumb2,thumb3,thumb4,thumb5,thumb6,thumb7,thumb8,thumb9,thumb10,thumb11,thumb12,thumb13,thumb14,thumb15,thumb16,thumb17,thumb18,thumb19,thumb20,thumb21,thumb22,thumb23,thumb24,thumb25,thumb26,thumb27,thumb28,thumb29,thumb30,thumb31]
 let menuimage = pickRandom(menuimages)
 let menureact = [global.reactmoji1,global.reactmoji2,global.reactmoji3,global.reactmoji4,global.reactmoji5,global.reactmoji6,global.reactmoji7,global.reactmoji8,global.reactmoji9,global.reactmoji10,global.reactmoji11,global.reactmoji12,global.reactmoji13,global.reactmoji14]
@@ -2996,8 +2996,8 @@ for (let i of msg.userReceipt) {
 let read = i.readTimestamp
 let unread = i.receiptTimestamp
 let waktu = read ? read : unread
-teks += `${global.themeemoji} @${i.userJid.split('@')[0]}\n`
-teks += ` ┗━${global.themeemoji} *Time :* ${moment(waktu * 1000).format('DD/MM/YY HH:mm:ss')} ${global.themeemoji} *Status :* ${read ? 'Read' : 'Sent'}\n\n`
+teks += `${globalthemeemoji} @${i.userJid.split('@')[0]}\n`
+teks += ` ┗━${globalthemeemoji} *Time :* ${moment(waktu * 1000).format('DD/MM/YY HH:mm:ss')} ${globalthemeemoji} *Status :* ${read ? 'Read' : 'Sent'}\n\n`
 }
 AnyaPika.sendTextWithMentions(m.chat, teks, m)
 }
@@ -5853,7 +5853,7 @@ let search = await yts(args.join(" "))
 let teks = '*| YOUTUBE SEARCH |*\n\n Result From '+text+'\n\n'
 let no = 1
 for (let i of search.all) {
-teks += `${global.themeemoji} No : ${no++}\n${global.themeemoji} Type : ${i.type}\n${global.themeemoji} Video ID : ${i.videoId}\n${global.themeemoji} Title : ${i.title}\n${global.themeemoji} Views : ${i.views}\n${global.themeemoji} Duration : ${i.timestamp}\n${global.themeemoji} Uploaded : ${i.ago}\n${global.themeemoji} Author : ${i.author.name}\n${global.themeemoji} Url : ${i.url}\n\n─────────────────\n\n`
+teks += `${globalthemeemoji} No : ${no++}\n${globalthemeemoji} Type : ${i.type}\n${globalthemeemoji} Video ID : ${i.videoId}\n${globalthemeemoji} Title : ${i.title}\n${globalthemeemoji} Views : ${i.views}\n${globalthemeemoji} Duration : ${i.timestamp}\n${globalthemeemoji} Uploaded : ${i.ago}\n${globalthemeemoji} Author : ${i.author.name}\n${globalthemeemoji} Url : ${i.url}\n\n─────────────────\n\n`
 }
 AnyaPika.sendMessage(m.chat, { image: { url: search.all[0].thumbnail },  caption: teks }, { quoted: m })
 }
@@ -5866,9 +5866,9 @@ let google = require('google-it')
 google({'query': args.join(" ")}).then(res => {
 let teks = `Google Search From : ${text}\n\n`
 for (let g of res) {
-teks += `${global.themeemoji} *Title* : ${g.title}\n`
-teks += `${global.themeemoji} *Description* : ${g.snippet}\n`
-teks += `${global.themeemoji} *Link* : ${g.link}\n\n────────────────────────\n\n`
+teks += `${globalthemeemoji} *Title* : ${g.title}\n`
+teks += `${globalthemeemoji} *Description* : ${g.snippet}\n`
+teks += `${globalthemeemoji} *Link* : ${g.link}\n\n────────────────────────\n\n`
 } 
 reply(teks)
 })
@@ -5889,8 +5889,8 @@ let buttonMessage = {
 image: { url: images },
 caption: `*| GOOGLE IMAGE |*
 
-${global.themeemoji} Query : ${text}
-${global.themeemoji} Media Url : ${images}`,
+${globalthemeemoji} Query : ${text}
+${globalthemeemoji} Media Url : ${images}`,
 footer: `${footer}`,
 buttons: buttons,
 headerType: 4,
@@ -5915,7 +5915,7 @@ try {
 hx.igstory(args[0]).then(async(resed) => {
 ini_anu = []
 anu_list = []
-textbv = `*| INSTAGRAM STORY |*\n\n${global.themeemoji} Username : ${resed.user.username ? resed.user.name : "undefined"}\n${global.themeemoji} Followers : ${resed.user.followers}`
+textbv = `*| INSTAGRAM STORY |*\n\n${globalthemeemoji} Username : ${resed.user.username ? resed.user.name : "undefined"}\n${globalthemeemoji} Followers : ${resed.user.followers}`
 urut = 1
 for (let i = 0; i < resed.medias.length; i++) {
 ini_anu.push({
@@ -5995,7 +5995,7 @@ try {
 hx.igdl(args[0]).then(async(resed) => {
 ini_anu = []
 anu_list = []
-textbv = `*| INSTAGRAM DOWNLOADER |*\n\n${global.themeemoji} Username : ${resed.user.username ? resed.user.name : "undefined"}\n${global.themeemoji} Followers : ${resed.user.followers}`
+textbv = `*| INSTAGRAM DOWNLOADER |*\n\n${globalthemeemoji} Username : ${resed.user.username ? resed.user.name : "undefined"}\n${globalthemeemoji} Followers : ${resed.user.followers}`
 urut = 1
 for (let i = 0; i < resed.medias.length; i++) {
 ini_anu.push({
@@ -7194,7 +7194,7 @@ case 'shortstoryx':{
 if (isBanChat) return reply(mess.banChat)
 if (!q) return reply(`*List*\n${prefix}shortstory Anak\n${prefix}shortstory Bahasa Daerah\n${prefix}shortstory Bahasa Inggris\n${prefix}shortstory Bahasa Jawa\n${prefix}shortstory Bahasa Sunda\n${prefix}shortstory Budaya\n${prefix}shortstory Cinta\n${prefix}shortstory Cinta Islami\n${prefix}shortstory Cinta Pertama\n${prefix}shortstory Cinta Romantis\n${prefix}shortstory Cinta Sedih\n${prefix}shortstory Cinta Segitiga\n${prefix}shortstory Cinta Sejati\n${prefix}shortstory Galau\n${prefix}shortstory Gokil\n${prefix}shortstory Inspiratif\n${prefix}shortstory Jepang\n${prefix}shortstory Kehidupan\n${prefix}shortstory Keluarga\n${prefix}shortstory Kisah Nyata\n${prefix}shortstory Korea\n${prefix}shortstory Kristen\n${prefix}shortstory Liburan\n${prefix}shortstory Lingkungan\n${prefix}shortstory Lucu\n${prefix}shortstory Malaysia\n${prefix}shortstory Mengharukan\n${prefix}shortstory Misteri\n${prefix}shortstory Motivasi\n${prefix}shortstory Nasihat\n${prefix}shortstory Nasionalisme\n${prefix}shortstory Olahraga\n${prefix}shortstory Patah Hati\n${prefix}shortstory Penantian\n${prefix}shortstory Pendidikan\n${prefix}shortstory Pengalaman Pribadi\n${prefix}shortstory Pengorbanan\n${prefix}shortstory Penyesalan\n${prefix}shortstory Perjuangan\n${prefix}shortstory Perpisahan\n${prefix}shortstory Persahabatan\n${prefix}shortstory Petualangan\n${prefix}shortstory Ramadhan\n${prefix}shortstory Remaja\n${prefix}shortstory Renungan\n${prefix}shortstory Rindu\n${prefix}shortstory Rohani\n${prefix}shortstory Romantis\n${prefix}shortstory Sastra\n${prefix}shortstory Sedih\n${prefix}shortstory Sejarah\n${prefix}shortstory Slice Of Life\n${prefix}shortstory Terjemahan\n${prefix}shortstory Thriller`)
 let cerpe = await cerpen(q)
-reply(`${global.themeemoji} _*Title :*_ ${cerpe.title}\n${global.themeemoji} _*Author :*_ ${cerpe.author}\n${global.themeemoji} _*Category :*_ ${cerpe.kategori}\n${global.themeemoji} _*Pass Moderation :*_ ${cerpe.lolos}\n${global.themeemoji} _*Story :*_\n${cerpe.cerita}`)
+reply(`${globalthemeemoji} _*Title :*_ ${cerpe.title}\n${globalthemeemoji} _*Author :*_ ${cerpe.author}\n${globalthemeemoji} _*Category :*_ ${cerpe.kategori}\n${globalthemeemoji} _*Pass Moderation :*_ ${cerpe.lolos}\n${globalthemeemoji} _*Story :*_\n${cerpe.cerita}`)
 }
 break
 case 'loveshortstoryx':{
@@ -7362,16 +7362,16 @@ let buttonMessage = {
 image: { url: anu.thumbnail },
 caption: `*| YOUTUBE PLAY |*
 
-${global.themeemoji}Title : ${anu.title}
-${global.themeemoji}Ext : Search
-${global.themeemoji}ID : ${anu.videoId}
-${global.themeemoji}Duration : ${anu.timestamp}
-${global.themeemoji}Viewers : ${anu.views}
-${global.themeemoji}Uploaded : ${anu.ago}
-${global.themeemoji}Author : ${anu.author.name}
-${global.themeemoji}Channel : ${anu.author.url}
-${global.themeemoji}Description : ${anu.description}
-${global.themeemoji}Url : ${anu.url}`,
+${globalthemeemoji}Title : ${anu.title}
+${globalthemeemoji}Ext : Search
+${globalthemeemoji}ID : ${anu.videoId}
+${globalthemeemoji}Duration : ${anu.timestamp}
+${globalthemeemoji}Viewers : ${anu.views}
+${globalthemeemoji}Uploaded : ${anu.ago}
+${globalthemeemoji}Author : ${anu.author.name}
+${globalthemeemoji}Channel : ${anu.author.url}
+${globalthemeemoji}Description : ${anu.description}
+${globalthemeemoji}Url : ${anu.url}`,
 footer: `${footer}`,
 buttons: buttons,
 headerType: 4,
@@ -7395,9 +7395,9 @@ try {
 hx.youtube(args[0]).then(async(res) => {
 textyt = `*| YOUTUBE DOWNLOADER |*
 
-${global.themeemoji} Title : ${res.title}
-${global.themeemoji} Size : ${res.size}
-${global.themeemoji} Quality : ${res.quality}
+${globalthemeemoji} Title : ${res.title}
+${globalthemeemoji} Size : ${res.size}
+${globalthemeemoji} Quality : ${res.quality}
 
 _Select video or audio and wait a while_`
 let buttons = [
