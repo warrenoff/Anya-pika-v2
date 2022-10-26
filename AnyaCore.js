b5e207b899e214186c62aab9ef97c28d9b3556f7
3.69_pika <106071901+Pika4O4@users.noreply.github.com>
2022-10-26 19:44:44 +0530

    Update AnyaCore.js

--- AnyaCore.js
+++ AnyaCore.js
@@ -7912,8 +7912,7 @@ break
 case 'description': 
 	   if (isBan) return reply(mess.ban)
 	if (isBanChat) return reply(mess.banChat)
-        reply(`ㅤ ㅤ࿙⳼ 𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗧𝗜𝗢𝗡 ⳹࿚\n\n${groupMetadata.desc}\n\n${footer}`)
-        reply(`𝘏𝘦𝘳𝘦'𝘴 𝘰𝘶𝘳 𝘨𝘳𝘰𝘶𝘱 𝘥𝘦𝘴𝘤𝘳𝘪𝘱𝘵𝘪𝘰𝘯 _${pushname}_ 𝘣𝘢𝘣𝘺❤️`)
+        reply(`ㅤ ㅤ࿙⳼ 𝗗𝗘𝗦𝗖𝗥𝗜𝗣𝗧𝗜𝗢𝗡 ⳹࿚\n\n${groupMetadata.desc}\n\n𝘏𝘦𝘳𝘦'𝘴 𝘰𝘶𝘳 𝘨𝘳𝘰𝘶𝘱 𝘥𝘦𝘴𝘤𝘳𝘪𝘱𝘵𝘪𝘰𝘯 _${pushname}_ 𝘣𝘢𝘣𝘺❤️\n\n${footer}`)
         break
 case 'antitag': {
 if (isBan) return reply(mess.ban)	 			
